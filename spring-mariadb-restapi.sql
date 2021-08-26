7727c6a2ad3993d57c6f47066339a9e1215809fe
蔡宗融(Dev3) <jonathan@iii.org.tw>
2021-08-26 14:29:05 +0000

    Update spring-mariadb-restapi.sql

--- spring-mariadb-restapi.sql
+++ spring-mariadb-restapi.sql
@@ -40,16 +40,16 @@ CREATE TABLE `employees` (
 -- 資料表結構 `hibernate_sequence`
 --
 
-CREATE TABLE `hibernate_sequence` (
-  `next_not_cached_value` bigint(21) NOT NULL,
-  `minimum_value` bigint(21) NOT NULL,
-  `maximum_value` bigint(21) NOT NULL,
-  `start_value` bigint(21) NOT NULL COMMENT 'start value when sequences is created or value if RESTART is used',
-  `increment` bigint(21) NOT NULL COMMENT 'increment value',
-  `cache_size` bigint(21) UNSIGNED NOT NULL,
-  `cycle_option` tinyint(1) UNSIGNED NOT NULL COMMENT '0 if no cycles are allowed, 1 if the sequence should begin a new cycle when maximum_value is passed',
-  `cycle_count` bigint(21) NOT NULL COMMENT 'How many cycles have been done'
-) ENGINE=InnoDB;
+-- CREATE TABLE `hibernate_sequence` (
+--   `next_not_cached_value` bigint(21) NOT NULL,
+--   `minimum_value` bigint(21) NOT NULL,
+--   `maximum_value` bigint(21) NOT NULL,
+--   `start_value` bigint(21) NOT NULL COMMENT 'start value when sequences is created or value if RESTART is used',
+--   `increment` bigint(21) NOT NULL COMMENT 'increment value',
+--   `cache_size` bigint(21) UNSIGNED NOT NULL,
+--   `cycle_option` tinyint(1) UNSIGNED NOT NULL COMMENT '0 if no cycles are allowed, 1 if the sequence should begin a new cycle when maximum_value is passed',
+--   `cycle_count` bigint(21) NOT NULL COMMENT 'How many cycles have been done'
+-- ) ENGINE=InnoDB;
 
 --
 -- 傾印資料表的資料 `hibernate_sequence`
