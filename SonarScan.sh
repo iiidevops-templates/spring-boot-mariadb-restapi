60b7e6989b05b37e0e06359cd74f29984796d7bf
jonathan <tryweb@ichiayi.com>
2022-01-25 16:58:49 +0800

    mvn 加上 -DskipTests

--- SonarScan.sh
+++ SonarScan.sh
@@ -8,8 +8,8 @@ curl -LO https://github.com/rancher/cli/releases/download/v2.4.6/rancher-linux-a
 rancher login ${rancher_url} -t ${rancher_api_token} --skip-verify && export SONAR_TOKEN=$(rancher kubectl get secret sonar-bot -n ${CICD_GIT_REPO_NAME} -o=go-template='{{index .data "sonar-token"}}' | base64 -d)
 
 echo '========== SonarQube(Maven) =========='
-cd app && mvn install
-mvn clean verify sonar:sonar -Dsonar.host.url=http://sonarqube-server-service.default:9000\
+cd app && mvn install -DskipTests
+mvn clean -DskipTests verify sonar:sonar -Dsonar.host.url=http://sonarqube-server-service.default:9000\
     -Dsonar.projectName=${CICD_GIT_REPO_NAME} -Dsonar.projectKey=${CICD_GIT_REPO_NAME}\
     -Dsonar.projectVersion=${CICD_GIT_BRANCH}:${CICD_GIT_COMMIT}\
 	-Dsonar.log.level=DEBUG -Dsonar.qualitygate.wait=true -Dsonar.qualitygate.timeout=600\
