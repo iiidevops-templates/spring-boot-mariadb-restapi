20f77d1efe576480cf8c77a6ed26ca5e302d7cde
mandsan <mandsan@kimo.com>
2022-04-28 14:28:33 +0800

    新增web

--- app/src/test/java/com/example/demo/DemoApplicationTests.java
+++ app/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.example.demo.Employee;
+import com.example.demo.EmployeeController;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	void contextLoads() {
+	void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
 
 }
