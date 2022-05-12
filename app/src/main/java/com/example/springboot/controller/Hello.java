60e757b399e5b2a7d1639b150223f365b5b81c0f
jonathan <tryweb@ichiayi.com>
2022-05-12 14:45:02 +0800

    minor update

--- app/src/main/java/com/example/springboot/controller/Hello.java
+++ app/src/main/java/com/example/springboot/controller/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String index() {
-		return "Greetings from Spring Boot RESTful API Sample project!, <a href='https://github.com/iiidevops-templates/spring-boot-mariadb-restapi/blob/master/README.md'>Click for more information</a>";
+		return "Greetings from Spring Boot - RESTful API Sample project! <a href='https://github.com/iiidevops-templates/spring-boot-mariadb-restapi/blob/master/README.md'>Click for more information</a>";
 	}
 
 }
